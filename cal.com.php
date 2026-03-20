2caa9f4591a37a140b411720898eef4b89c5539e
Shycoder <me@shycoder.com>
2026-03-21 00:42:01 +0100

    Updated readme

--- cal.com.php
+++ cal.com.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Cal.com
  * Description: Simplest and easiest way to embed Cal.com in WordPress.
- * Author: Cal.com, Inc & Shycoder
+ * Author: Cal.com, Simpma
  * Author URI: https://cal.com/
  * Version: 2.0.0
  * License: GPLv3 or later
